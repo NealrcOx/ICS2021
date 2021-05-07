c1d2fa325d34eca5beac7808afedfc8360458598
tracer-ics2017 <tracer@njuics.org>
2021-05-08 01:07:46 +0800

    >  compile 161520313 rancong Linux debian 4.19.0-16-686-pae #1 SMP Debian 4.19.181-1 (2021-03-19) i686 GNU/Linux  01:07:46 up  4:46,  6 users,  load average: 0.00, 0.00, 0.00 24f96299b5e3c3e8d1454b419d27efa1a309aa7

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -27,8 +27,9 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-#  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi#, edi;
-	 struct {
+/*	 rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi#, edi; 
+ */
+struct {
 		 	uint32_t eax;
 			uint32_t ecx;
 			uint32_t edx;
@@ -38,7 +39,8 @@ typedef struct {
 			uint32_t esi;
 			uint32_t edi;
 
-		 }
+		 };
+        };
  			 vaddr_t eip;
 
 } 
