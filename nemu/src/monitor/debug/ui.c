8eb0b020b8a8b241c784321df7390a519e8890e9
tracer-ics2017 <tracer@njuics.org>
2021-05-09 21:45:27 +0800

    >  compile 161520313 rancong Linux debian 4.19.0-16-686-pae #1 SMP Debian 4.19.181-1 (2021-03-19) i686 GNU/Linux  21:45:27 up 11:24,  7 users,  load average: 0.40, 0.40, 0.37 52c90ae9d8a3478bfae28e1ba93be99ae82e3509

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -160,12 +160,13 @@ static int cmd_x(char *args) {
 	int n = atoi(arg1);
 	vaddr_t addr;
 	sscanf(arg2,"%x",& addr);
-	vaddr_t byteAddr = addr;
-	printf("address        Dword block  ...  Byte sequence \n");
+//	vaddr_t byteAddr = addr;
+	printf("address        Dword block ...  Byte sequence \n");
 	for(int i = 0 ; i < n ; i++) {
-		printf("0x00%-x\t0x%-x\t  ...",addr + 4 * i,vaddr_read(addr + 4 * i, 4));
+		printf("0x00%-x\t0x%-x\t... ",addr + 4 * i,vaddr_read(addr + 4 * i, 4));
+		vaddr_t byteAddr = addr + 4 * i;
 		for(int j = 0 ; j < 4 ; j++) {
-			printf("%2x ", vaddr_read(byteAddr + j, 1));
+			printf("%2x ", vaddr_read(byteAddr, 1));
 		}
 		printf("\n");
 	}
