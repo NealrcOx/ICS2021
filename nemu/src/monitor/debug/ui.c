4935766511a5f3e13785f058be9779bc62659622
tracer-ics2017 <tracer@njuics.org>
2021-05-09 18:57:42 +0800

    >  compile 161520313 rancong Linux debian 4.19.0-16-686-pae #1 SMP Debian 4.19.181-1 (2021-03-19) i686 GNU/Linux  18:57:42 up  8:36,  7 users,  load average: 0.15, 0.32, 0.40 9490f190460dac2e37396ed5d089bf9a8dba4885

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -164,9 +164,10 @@ static int cmd_x(char *args) {
 		return 0;
 	}
 		int n = atoi(arg1);  //atoi():this function can make string to number, 
+		printf("%0x-10x %d\n", addr, n);
 		// n is the address steps
-		int i;
-		for(i = 0 ; i < n ; i+=4){	//the address is 4 bytes
+//		int i;
+//		for(i = 0 ; i < n ; i+=4){	//the address is 4 bytes
 	/*		if(i + 4 < n){
 			printf("0x%-10x : 0x%-15x\n0x%-15x\n0x%-15x\n0x%-15x\n",addr, vaddr_read(addr, 4),vaddr_read(addr + 4, 4), vaddr_read(addr + 8 , 4), vaddr_read(addr + 12, 4));
 		}
@@ -180,8 +181,8 @@ static int cmd_x(char *args) {
 			printf("\n");
 		}
 			addr += 16; */
-			printf("0x%-10x : 0x%-10x\n",addr, paddr_read(addr+ (4 * i), 4));
-		}
+//			printf("0x%-10x : 0x%-10x\n",addr, paddr_read(addr+ (4 * i), 4));
+//		}
 			
 	//	}
 
