66d2071292ceba019f331d49914d21421ab275c0
161520313-Ran Cong <nealrancong@foxmail.com>
2021-05-16 23:32:11 +0800

    coding watchpint.c

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,20 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+//new a watchpint
+WP * new_wp(){
+if(free_ == NULL){
+	//log("Not has enough watchpoint\n");
+	assert(0);
+	return NULL;
+}
+	WP * wp = free_;
+	free_ = free_->next;
+
+	wp->next = head;
+	head = wp;
+
+	return head;
+	}
+
 
