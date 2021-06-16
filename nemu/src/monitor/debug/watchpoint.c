07d9c190bfb7648a5d26a4e2bde4112e74109571
tracer-ics2017 <tracer@njuics.org>
2021-06-17 01:04:03 +0800

    >  compile 161520313 rancong Linux debian 4.19.0-16-686-pae #1 SMP Debian 4.19.181-1 (2021-03-19) i686 GNU/Linux  01:04:03 up 29 min,  3 users,  load average: 0.38, 0.40, 0.33 1e8e133a673545cb44b460e6e24bdbdd9a09e454

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -27,32 +27,29 @@ if(free_ == NULL){
 	assert(0);
 	return NULL;
 }
-	WP * wp = free_;
+	int Num = free_ -> NO;
 	free_ = free_->next;
-
-	wp->next = head;
-	head = wp;
-
+	wp_pool[Num].next =head;
+	head = &wp_pool[Num];
 	return head;
 	}
 
 //free a watchpoint NO is n
 void free_wp(int n){
+	WP *wp;
 	if(head->NO == n){
-		WP * wp = head->next;
+		wp = head->next;
 		head->next = free_;
 		free_ = head;
 		head = wp;
-		return ;
+	//	return ;
 	}
-	WP * pwt = NULL;
-	for(pwt = head ; pwt->next != NULL ; pwt = pwt->next){
-		if(pwt->next->NO == n){
-			WP * wp = pwt->next;
-			pwt->next = wp->next;
-			wp->next = free_;
-			free_ = wp;
-			return ;
-		}
+	else{
+	WP * wp1,* wp2,* wp3;
+	for(wp1 = head, wp2 = head->next; wp2->NO != n; wp1 = wp1->next,wp2 = wp2->next);
+	wp3 = &wp_pool[n];
+	wp1 -> next = wp3 ->next;
+	wp3 -> next = free_;
+	free_ = wp3;
 	}
 }
